824bf3b5c3401cbb83e94917d94131c5009d0fde
Tomoharu Kawata <tomkawata@microsoft.com>
2025-12-27 15:41:14 +0900

    first commit

---
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/NotificationSettings.kt
@@ -0,0 +1,6 @@
+package com.google.firebase.quickstart.fcm.kotlin
+
+object NotificationSettings {
+    var HubName: String = "tomkawatanh"
+    var HubListenConnectionString: String = "PEjNpmMBzfmhhcXJ85dHC+uuKXy3Ych8+PCuKX4oW+0="
+}
